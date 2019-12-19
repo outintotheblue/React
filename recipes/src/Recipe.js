08e2507ef30741ce492c79094009d7bf9649dd0e
unknown <martinaoyhenard@gmail.com>
2019-12-19 12:40:01 +0100

    new css

--- recipes/src/Recipe.js
+++ recipes/src/Recipe.js
@@ -7,7 +7,7 @@ const Recipe = ({title,calories, img, ingredients}) => {
         <li>{ingredient.text}</li>
     ))}
     </ol>
-    <p>{calories}</p>
+    <p>Calories: {calories}</p>
     <img src={img} alt =''/>
 
     </div>
